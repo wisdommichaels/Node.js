fc728be3fcec7b1ecbad1033ee19338385a10504
wisdomMichaels <ijarwisdom@gmail.com>
2024-07-31 03:08:21 +0100

    virsion 1.3

--- hello_world.js
+++ hello_world.js
@@ -2,12 +2,23 @@
 // console.log("HELLO WORLD");
 
 // Write a program that runs on port 3000 and responds with  '<h1>Hello Node!!!!</h1>\n' when someone reaches http://localhost:3000
-const http = require ("http")
-http.createServer(function(request, response) {
+// const http = require ("http")
+// http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/html"});
+//     response.writeHead(200, {"Content-Type": "text/html"});
 
-    response.end("<h1>Hello Node!!!!</h1>\n");
+//     response.end("<h1>Hello Node!!!!</h1>\n");
+
+// }).listen(3000);
+// console.log("Server running at http://localhost:3000");
+
+// create a program that reads and console.log data from hello.txt
+const fs = require ("fs")
+fs.readFile("welcome.txt", function(error, data){
+    if(error){
+        console.log(error.stack);
+        return;
+    };
+    console.log(data.toString());
     
-}).listen(3000);
-console.log("Server running at http://localhost:3000");
+});
