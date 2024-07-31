44b4cb33761ccfba9b512c41202c1f49c7e5c518
wisdomMichaels <ijarwisdom@gmail.com>
2024-07-31 02:42:46 +0100

    initial Commit

--- hello_world.js
+++ hello_world.js
@@ -1,3 +1,13 @@
 // Write a program to print "HELLO WORLD" to the console
-console.log("HELLO WORLD");
+// console.log("HELLO WORLD");
 
+// Write a program that runs on port 3000 and responds with  '<h1>Hello Node!!!!</h1>\n' when someone reaches http://localhost:3000
+const http = require ("http")
+http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+
+    response.end("<h1>Hello Node!!!!</h1>\n");
+    
+}).listen(3000);
+console.log("Server running at http://localhost:3000");
